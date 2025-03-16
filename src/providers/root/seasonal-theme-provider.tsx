d25284c8b0399ecf4a2f3d5ecdbc126d1ec9dba9
huangyan321 <1604549268@qq.com>
2025-03-16 20:33:18 +0800

    chore: 是时候移除新年主题了

--- src/providers/root/seasonal-theme-provider.tsx
+++ src/providers/root/seasonal-theme-provider.tsx
@@ -1,43 +1,43 @@
 'use client'
-import { useTheme } from 'next-themes'
+// import { useTheme } from 'next-themes'
 import * as React from 'react'
-import { flushSync } from 'react-dom'
+// import { flushSync } from 'react-dom'
 
-import Lantern from '~/components/seasonal/Lantern'
-import Snowfall from '~/components/seasonal/SnowFall'
-import { useIsMountedState } from '~/hooks/common/use-is-mounted'
-import { transitionViewIfSupported } from '~/lib/dom'
-import { toast } from '~/lib/toast'
+// import Lantern from '~/components/seasonal/Lantern'
+// import Snowfall from '~/components/seasonal/SnowFall'
+// import { useIsMountedState } from '~/hooks/common/use-is-mounted'
+// import { transitionViewIfSupported } from '~/lib/dom'
+// import { toast } from '~/lib/toast'
 
 export function SeasonalThemeProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const { setTheme, resolvedTheme } = useTheme()
-  const isMounted = useIsMountedState()
+  // const { setTheme, resolvedTheme } = useTheme()
+  // const isMounted = useIsMountedState()
 
-  const buildThemeTransition = (theme: 'light' | 'dark' | 'system') => {
-    transitionViewIfSupported(() => {
-      flushSync(() => setTheme(theme))
-    })
-  }
-  React.useEffect(() => {
-    if (!isMounted) return
+  // const buildThemeTransition = (theme: 'light' | 'dark' | 'system') => {
+  //   transitionViewIfSupported(() => {
+  //     flushSync(() => setTheme(theme))
+  //   })
+  // }
+  // React.useEffect(() => {
+  //   if (!isMounted) return
 
-    if (resolvedTheme !== 'dark') {
-      toast('新年主题已推送，点我立即体验！', 'info', {
-        onClick: () => {
-          buildThemeTransition('dark')
-        },
-      })
-    }
-  }, [isMounted])
+  //   if (resolvedTheme !== 'dark') {
+  //     toast('新年主题已推送，点我立即体验！', 'info', {
+  //       onClick: () => {
+  //         buildThemeTransition('dark')
+  //       },
+  //     })
+  //   }
+  // }, [isMounted])
 
   return (
     <>
-      <Snowfall maxFlake={10} flakeSize={8} fallSpeed={0.2} />
-      <Lantern />
+      {/* <Snowfall maxFlake={10} flakeSize={8} fallSpeed={0.2} />
+      <Lantern /> */}
       <div>{children}</div>
     </>
   )
