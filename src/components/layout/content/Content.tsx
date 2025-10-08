1510e0a22068dc4e4032078c76f94bd3d75debe5
huangyan321 <1604549268@qq.com>
2025-10-08 21:57:48 +0800

    fix: Remove inline styles and background image from Content component

--- src/components/layout/content/Content.tsx
+++ src/components/layout/content/Content.tsx
@@ -1,11 +1,6 @@
 export const Content: Component = ({ children }) => {
   return (
-    <main
-      style={{
-        backgroundSize: '60%',
-      }}
-      className="fill-content relative bg-[url(https://i.pinimg.com/1200x/54/fe/d8/54fed8f86ad1f0fd04b7ee7ac0cdc5d6.jpg)] bg-repeat px-4 pt-[4.5rem] dark:bg-none md:px-0"
-    >
+    <main className="fill-content relative z-[1] px-4 pt-[4.5rem] md:px-0">
       {children}
     </main>
   )
